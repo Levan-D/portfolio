bffd10dab29958d7a19eda1c0ecd2723e8176377
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-06-26 23:20:46 +0400

    scroll down animation

--- app/(homepage)/about/components/ScrollBtn.tsx
+++ app/(homepage)/about/components/ScrollBtn.tsx
@@ -11,12 +11,20 @@ export default function ScrollBtn() {
     element?.scrollIntoView({ behavior: "smooth" })
   }
   return (
-    <div className="group absolute -bottom-28 left-1/2 hidden -translate-x-1/2 rounded-full    duration-500 hover:p-14 md:block">
+    <div className="scrollWrap group absolute -bottom-28 left-1/2 hidden -translate-x-1/2 rounded-full    duration-500 hover:p-14 md:block">
       <button
         onClick={scrollToElementSmooth}
         className="rounded-full border-2 border-teal-400 p-28 duration-500 group-hover:p-14  group-hover:text-slate-950 group-hover:[background:linear-gradient(135deg,_rgba(94,_234,_212,_1)_20%,rgba(45,_212,_191,_1)_80%)] "
       >
-        <Icon path={mdiArrowDown} size={2} />
+        <div className="arrow transition-opacity group-hover:opacity-0">
+          <Icon path={mdiArrowDown} size={2} />
+        </div>
+        <div className="arrowDelay">
+          <Icon path={mdiArrowDown} size={2} />
+        </div>
+        <div className="arrowDelaySecond">
+          <Icon path={mdiArrowDown} size={2} />
+        </div>
       </button>
     </div>
   )
